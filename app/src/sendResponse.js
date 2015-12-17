a62e545ae2c6ae0e739b66208ce04f0126b87ee1
Dimitri van Hees <dimitri@apiwise.nl>
2015-12-17 15:32:33 +0000

    Municipality HTML + JSON-LD

--- app/src/sendResponse.js
+++ app/src/sendResponse.js
@@ -1,4 +1,5 @@
-var jade = require('jade');
+var jade = require('jade'),
+  geojson2schema = require('geojson2schema');
 
 module.exports = function(req, res, data, status) {
   if (status !== undefined) {
@@ -7,9 +8,22 @@ module.exports = function(req, res, data, status) {
 
   res.format({
     'text/html': function() {
-      res.send(
-        jade.renderFile('./templates/base.jade', data)
-      );
+      try {
+        res.send(
+          jade.renderFile('./templates/base.jade', {
+            object: data,
+            jsonld: JSON.stringify({
+              '@context': 'http://schema.org',
+              '@type': 'Place',
+              geo: geojson2schema({
+                geoJson: data.geometry
+              })
+            })
+          })
+        );
+      } catch(err) {
+        console.error(err);
+      }
     },
     'application/json': function() {
       res.json(data);

5a62afaafcbd1361c62056dc57d98b4bb723fae3
Dimitri van Hees <dimitri@apiwise.nl>
2015-12-18 11:11:12 +0000

    Added properties

--- app/src/sendResponse.js
+++ app/src/sendResponse.js
@@ -12,12 +12,18 @@ module.exports = function(req, res, data, status) {
         res.send(
           jade.renderFile('./templates/base.jade', {
             object: data,
+            title: (
+              data.properties['BU_NAAM'] ? data.properties['BU_NAAM'] : (
+                data.properties['WK_NAAM'] ? data.properties['WK_NAAM'] : (
+                  data.properties['GM_NAAM'] ? data.properties['GM_NAAM'] : 'Geo4Web'
+                )
+              )
+            ),
             jsonld: {
               '@context': 'http://schema.org',
               '@type': 'Place',
               geo: geojson2schema({
-                geoJson: data.geometry,
-                tolerance: 0.01
+                geoJson: data.geometry
               })
             }
           })

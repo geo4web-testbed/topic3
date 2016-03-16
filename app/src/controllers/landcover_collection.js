80188638745c317eb6bc25cf16ecf8d65ac7dae2
Joost Farla <joost@apiwise.nl>
2016-03-16 13:09:58 +0000

    Added Swagger spec + full text search

--- app/src/controllers/landcover_collection.js
+++ app/src/controllers/landcover_collection.js
@@ -3,25 +3,27 @@ var createError = require('http-errors'),
   sendResponse = require('../sendResponse');
 
 module.exports = function(esClient) {
-  var params = {
-    index: 'geo4web',
-    type: 'bestemmingsplangebied',
-    sort: '_self_name',
-    _source: ['_self_name']
-  };
-
   return function(req, res) {
     var currentPage = Number(req.query.page) || 1;
 
-    params.size = 1000;
-    params._source = ['_self_name'];
+    var params = {
+      index: 'geo4web',
+      type: 'bestemmingsplangebied',
+      sort: '_self_name'
+    };
 
     if (req.accepts(['text/html', 'application/json', 'application/ld+json']).indexOf('json') >= 0) {
       params.size = 20;
-      delete params._source;
+    } else {
+      params.size = 1000;
+      params._source = ['_self_name'];
     }
 
     params.from = (currentPage - 1) * params.size;
+
+    if (req.query.q) {
+      params.q = req.query.q;
+    }
 
     esClient.search(params).then(function(result) {
       sendResponse(req, res, 'landcover_collection', {
